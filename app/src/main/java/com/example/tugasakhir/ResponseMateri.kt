41f1fd9d2d213cc5e0dfdf019dde4716d032a35a
fadjargintin <fajardwitama03@gmail.com>
2023-12-17 22:32:47 +0700

    update

---
+++ app/src/main/java/com/example/tugasakhir/ResponseMateri.kt
@@ -0,0 +1,42 @@
+package com.example.tugasakhir
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseMateri(
+
+	@field:SerializedName("data")
+	val data: List<DataItem?>? = null,
+
+	@field:SerializedName("count")
+	val count: Int? = null,
+
+	@field:SerializedName("message")
+	val message: String? = null,
+
+	@field:SerializedName("status")
+	val status: Int? = null
+)
+
+data class DataItem(
+
+	@field:SerializedName("materi")
+	val materi: String? = null,
+
+	@field:SerializedName("updated_at")
+	val updatedAt: String? = null,
+
+	@field:SerializedName("namaFile")
+	val namaFile: String? = null,
+
+	@field:SerializedName("created_at")
+	val createdAt: String? = null,
+
+	@field:SerializedName("id")
+	val id: Int? = null,
+
+	@field:SerializedName("judul")
+	val judul: String? = null,
+
+	@field:SerializedName("link_vd")
+	val linkVd: String? = null
+)
---
+++ app/src/main/java/com/example/tugasakhir/api/ApiMateriConfig.kt
@@ -0,0 +1,4 @@
+package com.example.tugasakhir.api
+
+object ApiMateriConfig {
+}
